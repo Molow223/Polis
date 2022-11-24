b8bff742b2f96b3ce5257e812d254286fe25a124
Molow223 <molow223@gmail.com>
2022-11-24 17:50:05 +0500

    StartPolis

---
+++ script.js
@@ -0,0 +1,86 @@
+
+/* Устанавливаем индекс слайда по умолчанию */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд*/
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Уменьшает индекс на 1 — показываем предыдущий слайд*/
+function previousSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливаем текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Функция перелистывания */
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("item");
+    
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+  
+  /* Проходим по каждому слайду в цикле for */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }   
+    slides[slideIndex - 1].style.display = "block"; 
+}
+
+let cells = document.querySelectorAll('#field td');
+start(cells);
+
+function start(cells) {
+	let i = 0;
+	
+	for (let cell of cells) {
+		cell.addEventListener('click', function step() {
+			this.textContent = ['X', 'O'][i % 2];
+			this.removeEventListener('click', step);
+			
+			if (isVictory(cells)) {
+				alert(this.textContent); 
+                // выводим имя победителя
+			} else if (i == 8) {
+				alert('ничья');
+			}
+			
+			i++;
+		});
+	}
+}
+function isVictory(cells) {
+	let combs = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let comb of combs) {
+		if (
+			cells[comb[0]].textContent == cells[comb[1]].textContent &&
+			cells[comb[1]].textContent == cells[comb[2]].textContent &&
+			cells[comb[0]].textContent != ''
+		) {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
